ba307db63724a72fe8cf46dffba9835c05875b8a
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-02-12 20:29:37 +0900

    Create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# m3_component_color_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/dr1009/app/m3_component_color_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dr1009.app.m3_component_color_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
